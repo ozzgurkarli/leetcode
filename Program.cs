3ff4b0bd63720e9f522ac82ca57353cdc0989961
ozzgurkarli <ozzgurkarli2@gmail.com>
2025-03-17 12:31:02 +0300

    2206

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+    }
+
+    private bool solution_2206_(int[] nums)
+    {
+        List<int> pairedIndexes = new List<int>();
+        int pairIndex;
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if(pairedIndexes.Contains(i))
+                continue;
+
+            pairIndex = findPair(nums, nums[i], i + 1, pairedIndexes);
+
+            if (pairIndex == -1)
+            {
+                return false;
+            }
+            
+            pairedIndexes.Add(pairIndex);
+        }
+
+        return true;
+    }
+
+    private int findPair(int[] nums, int num, int startIndex, List<int> pairedIndexes)
+    {
+        for (int i = startIndex; i < nums.Length; i++)
+        {
+            if (nums[i] == num && !pairedIndexes.Contains(i))
+                return i;
+        }
+
+        return -1;
+    }
+}
