7cec2dd2555007c3e0a0b90b2120f313a8074bf4
ozzgurkarli <ozzgurkarli2@gmail.com>
2025-04-27 22:36:20 +0300

    fix

--- Solutions.cs
+++ Solutions.cs
@@ -102,7 +102,7 @@ public partial class Leetcode
         
         for (int i = 0; i < nums.Length - 2; i++)
         {
-            if (nums[i + 1] / 2 == nums[i] + nums[i + 2])
+            if (nums[i + 1] == (nums[i] + nums[i + 2]) * 2)
                 output++;
         }
 
